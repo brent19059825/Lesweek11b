856987da27a2297d1835568ba4c32f6a605e5877
brent19059825 <Kush!420>
2020-11-20 14:39:34 +0100

    Add project files.

---
+++ Lesweek11b/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesweek11b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
